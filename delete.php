2a1796362729eeec6038799cf638188f26310f52
Eng <simplyhl@hotmail.com>
2023-08-23 17:53:04 +0800

    pre-edit

--- delete.php
+++ delete.php
@@ -1,8 +1,8 @@
 <html><head></head><body style='align:center'>
-<form action="update.php" method="post">
+<form method="post">
 
 <?php
-  echo"Hello World";
+  echo"Caution! to delete...";
 $link=mysqli_connect("localhost","root","spot1234","school") or die(mysql_error());
 
 $query="SELECT * FROM student WHERE sno='".$_GET['sno']."'";
@@ -16,8 +16,8 @@ ID<input type="text" name="sno" value=<?=$row['sno']?>><br>
 Your Name <input type="text" name="sname" value=<?=$row['sname']?>></input><br>
 Gender <input type="radio" name="ssex" value=<??>>Male</input><br>
 <input type="radio" name="ssex">Female</input><br>
-<input type="submit" value="Go"</input>
-<input type="reset" value="Clear"</input>
+<input type="submit" value="Delete"</input>
+
 
 <?php
 }
